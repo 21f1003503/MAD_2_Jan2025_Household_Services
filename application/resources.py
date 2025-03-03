16b34808548427532afe6968f45d52047214bea0
21f1003503 <21f1003503@ds.study.iitm.ac.in>
2025-03-04 01:32:16 +0530

    Updated routes and added resources for service request creation

--- application/resources.py
+++ application/resources.py
@@ -1,22 +1,73 @@
 from flask_restful import Api, Resource, reqparse
 from .models import *
-from flask_security import auth_required, roles_required, roles_accepted
+from flask_security import auth_required, roles_required, roles_accepted, current_user
 
 api = Api()
 
 parser = reqparse.RequestParser()
-
-parser.add_argument()
+parser.add_argument('serviceID') 
+parser.add_argument('spID') 
+parser.add_argument('service_status') 
+parser.add_argument('date_of_req')
+parser.add_argument('date_of_completion') 
+parser.add_argument('remarks') 
+parser.add_argument('rating') 
 
 def roles_list(roles):
-    roles_list = []
+    role_list = []
     for role in roles:
-        roles_list.append(role.name)
-    return roles_list
+        role_list.append(role.name)
+    return role_list
 
-class ServiceAPI(Resource):
+class ServiceRequestApi(Resource):
+    @auth_required('token')
+    @roles_accepted('admin', 'customer', 'service_professional')
     def get(self):
-        services = []
-        services_json = []
+        service_requests = []
+        service_requests_json = []
 
+        if "admin" in roles_list(current_user.roles):
+            service_requests = Service_Request.query.all()
+        else:
+            service_requests = current_user.serv_reqs
         
+        for service_req in service_requests:
+            this_serv_req = {}
+            this_serv_req["s_reqID"] = service_req.s_reqID
+            this_serv_req["service"] = service_req.serviceID
+            this_serv_req["customer"] = service_req.customerID
+            this_serv_req["service_professional"] = service_req.spID
+            this_serv_req["service_status"] = service_req.service_status
+            this_serv_req["date_of_request"] = service_req.date_of_req
+            this_serv_req["date_of_completion"] = service_req.date_of_completion
+            this_serv_req["remarks"] = service_req.remarks
+            this_serv_req["rating"] = service_req.rating
+            service_requests_json.append(this_serv_req)
+            
+        if service_requests_json:
+            return service_requests_json
+
+        return {
+            "message": "No Service Request Found!!!"
+        }, 404
+    
+    @auth_required('token')
+    @roles_required('customer') # used roles_required here instead of roles_accepted because only customer can create a new service request
+    def post(self):
+        args = parser.parse_args()
+        try:
+            service_req = Service_Request(serviceID = args["serviceID"],
+                                        customerID = current_user.id,
+                                        date_of_req = args["date_of_req"],
+                                        service_status = args['service_status'])
+            db.session.add(service_req)
+            db.session.commit()
+            return {
+                "message": "Service Request Created Successfully!!!"
+            }
+        except:
+            return {
+                "message": "One or More Required Fields Are Missing!!!"
+            }, 400
+    
+api.add_resource(ServiceRequestApi, '/api/service_request/get', '/api/service_request/create')
--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,15 @@
 from .database import db
 from .models import User, Role
 
-from flask import current_app as app, jsonify, request
+from flask import current_app as app, jsonify, request, render_template
 from flask_security import auth_required, roles_required, roles_accepted, current_user, hash_password
+
+@app.route('/', methods = ['GET'])
+def home():
+    #return "<h1>This is home page</h1>"
+    return render_template('index.html')
+
+#"@app.route('/login')"
 
 @app.route('/api/admin')
 @auth_required('token')
@@ -19,19 +26,24 @@ def customer_home():
     customer = current_user
     return jsonify({
         "Name": customer.name,
-        "E-mail": customer.email,
+        "E-mail": customer.username,
         "Password": customer.password
     })
 
 @app.post('/api/cu_registration')
 def cu_registration():
     credentials = request.get_json()
-
+        
     if not app.security.datastore.find_user(username = credentials["username"]):
         app.security.datastore.create_user(username = credentials["username"],
-                                           full_name = credentials["full_name"],
                                            password = hash_password(credentials["password"]),
-                                           roles = ["customer"])
+                                           full_name = credentials["full_name"],
+                                           roles = ["customer"],
+                                           cu_address = credentials["cu_address"],
+                                           pincode = credentials["pincode"],
+                                           phone_number = credentials["phone_number"],
+                                           )
+
         db.session.commit()
         return jsonify({
             "message": "Customer Created Successfully!!!"
@@ -44,12 +56,17 @@ def cu_registration():
 @app.post('/api/sp_registration')
 def sp_registration():
     credentials = request.get_json()
-
+        
     if not app.security.datastore.find_user(username = credentials["username"]):
         app.security.datastore.create_user(username = credentials["username"],
-                                           full_name = credentials["full_name"],
                                            password = hash_password(credentials["password"]),
-                                           roles = ["service_professional"])
+                                           full_name = credentials["full_name"],
+                                           roles = ["service_professional"],
+                                           pincode = credentials["pincode"],
+                                           phone_number = credentials["phone_number"],
+                                           sp_experience = credentials["sp_experience"],
+                                           )
+
         db.session.commit()
         return jsonify({
             "message": "Service Professional Created Successfully!!!"
